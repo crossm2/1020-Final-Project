02c1355bdb3c7c4a2469b9940094270875093ad4
crossm2 <crossm2@seattleu.edu>
2016-03-02 18:25:13 -0500

    saving progress images and modal

--- js/main.js
+++ js/main.js
@@ -56,5 +56,32 @@ marker.bindPopup("<b>Marlie's Magic Morsels</b><br>Our Biggest Vegan Selection")
 var marker3 = L.marker([47.613509,-122.330145],{icon: cupcakeIcon}).addTo(map);
 marker.bindPopup("<b>Marlie's Magic Morsels</b><br>Our Most Popular Store");
 //adding markers to the map 
+	
+	
+	// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal 
+btn.onclick = function() {
+    modal.style.display = "block";
+};
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+};
 
 });
