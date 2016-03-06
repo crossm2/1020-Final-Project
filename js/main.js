fe18aed77e9cb14bd35b0509963c1b22379b3b0a
crossm2 <crossm2@seattleu.edu>
2016-03-06 13:25:24 -0500

    saving progress added links

--- js/main.js
+++ js/main.js
@@ -83,5 +83,43 @@ window.onclick = function(event) {
         modal.style.display = "none";
     }
 };
+	
+	    // Form validation
+     $('#order-form').validate({
+         submitHandler: function(form) {
+             // If form is valid, submit it!
+             form.submit();
+         },
+         rules: {
+             "your-name": {
+                 required: true,
+                 maxlength: 128
+             },
+             "your-state": {
+                 required: true,
+                 maxlength: 2
+            },
+             "your-zip": {
+               required: true,
+                 maxlength: 5,
+                 digits: true
+            },
+             "card-holder-name": {
+                 required: true,
+                 maxlength: 128
+             },
+            "card-number": {
+                 required: true,
+                 creditcard: true
+             },
+             "cvv": {
+                 required: true,
+                 maxlength: 3,
+                 digits: true
+             }
+         }
+     });
+    $('label span.glyphicon').tooltip();
+
 
 });
